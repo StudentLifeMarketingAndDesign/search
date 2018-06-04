d19f9fab2075ae144fe4dc14ff3420d48b72ab75
Dustin Quam <quamsta@gmail.com>
2018-06-04 10:30:08 -0500

    enable HawkIDs

--- mysite/_config.php
+++ mysite/_config.php
@@ -21,8 +21,8 @@ if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
 if(Director::isLive()) {
 	Director::forceSSL();
 }
-// Authenticator::unregister('MemberAuthenticator');
-// Authenticator::set_default_authenticator('SAMLAuthenticator');
-Authenticator::unregister('SAMLAuthenticator');
-Authenticator::set_default_authenticator('MemberAuthenticator');
+Authenticator::unregister('MemberAuthenticator');
+Authenticator::set_default_authenticator('SAMLAuthenticator');
+// Authenticator::unregister('SAMLAuthenticator');
+// Authenticator::set_default_authenticator('MemberAuthenticator');
 SSViewer::set_theme('division-subtheme');
